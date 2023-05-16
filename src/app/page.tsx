b129ee14eb8daad68f6c234ea64a7247a2c42ed9
hnakae <nakae27@gmail.com>
2023-05-15 21:05:59 -0700

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { prisma } from "@/lib/prisma";
+// import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { User } from "./user";
+// import { User } from "./user";
 import { LoginButton, LogoutButton } from "./auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  const user = await prisma.user.findFirst({
-    where: {
-      email: "test@test.io",
-    },
-  });
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     email: "test@test.io",
+  //   },
+  // });
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-100">
       {session !== null ? <LogoutButton /> : <LoginButton />}
 
-      <div className="mt-6">Hello, {user?.name}</div>
+      {/* <div className="mt-6">Hello, {user?.name}</div> */}
       {/* <h2>Server Call</h2>
       <pre>{JSON.stringify(session)}</pre>
       <h2>Client Call</h2>
