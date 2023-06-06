4db7f2e525af957b112a432057b7d864812ad5d8
hnakae <nakae27@gmail.com>
2023-06-06 04:48:19 -0700

    add to dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 
 import Course from "@/components/ui/course/course";
+import Image from "next/image";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -14,23 +15,21 @@ export default async function Dashboard() {
   const courses = await prisma.course.findMany({});
   // console.log(user);
   return (
-    <div className="outline  min-h-screen pt-[64px] space-y-6">
+    <div className="  min-h-screen py-[64px] space-y-6">
       {/* <Image src={profile} alt="image" width="500" height="500" /> */}
 
-      <div className=" flex flex-col justify-center items-center pt-6">
+      <div className=" flex flex-col justify-center items-center pt-6 space-y-4">
         <Card data={session?.user?.email} />
+        <Image src="/hikaru.jpg" alt="" width={850} height={100} />
+        <div className="flex justify-center items-center w-[80%] space-x-4 flex-grow-0">
+          <Card data="Rank: [1d]" />
+          <Card data="Goal: [2d]" />
+          <Card data="challenges: checkmarked calendar" />
+          <Card data="Tsumego Categories" />
+          <Card data="Kifu Library" />
+          <Card data="Game Reviews" />
+        </div>
       </div>
-      {/* <div className="grid grid-cols-4 gap-4">
-        <Card data="Rank: [1d]" />
-        <Card data="Goal: [2d]" />
-        <Card data="My Courses" />
-        <Card data="challenges: checkmarked calendar" />
-        <Card data="Tsumego Categories" />
-        <Card data="Game Reviews" />
-        <Card data="Kifu Library" />
-        <Card data="AI Suggestions" />
-      </div> */}
-      {/* <div className="grid grid-cols-1 gap-4"> */}
       <div className="text-center font-bold text-4xl py-6">My Courses</div>
       {courses.map((course: any) => (
         <Course
@@ -40,7 +39,6 @@ export default async function Dashboard() {
           image={course.image}
         />
       ))}
-      {/* </div> */}
     </div>
   );
 }
--- src/components/ui/card/index.tsx
+++ src/components/ui/card/index.tsx
@@ -1,9 +1,7 @@
 const Card = ({ data }: any) => {
   return (
-    <div className="flex-shrink-0 w-[80vw] bg-white rounded-lg shadow-md">
-      <div className="flex justify-center items-center h-full text-center py-4 ">
-        {data}
-      </div>
+    <div className="w-[80%] bg-white rounded-lg shadow-md">
+      <div className="h-full text-center py-4 text-black ">{data}</div>
     </div>
   );
 };
