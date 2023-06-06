978a06d5e415c4d99458fb90d47dd78395484fb0
hnakae <nakae27@gmail.com>
2023-06-06 04:25:20 -0700

    get plus

--- src/app/(app)/getplus/page.tsx
+++ src/app/(app)/getplus/page.tsx
@@ -9,8 +9,8 @@ export default async function Getplus() {
       <div className="flex justify-center items-center px-12">
         <div className=" overflow-hidden  relative h-full rounded-lg ">
           <Image src={pic} alt="go player" className="z-0 " />
-          <div className="absolute bottom-0 left-0 z-10 flex flex-col justify-center items-center p-12 space-y-4 bg-opaque h-full w-[40%] text-xs">
-            <div>
+          <div className="z-10 flex flex-col  bg-opaque h-full w-[40%] text-xs">
+            <div className="bg-opaque space-y-4 h-[60%] w-[40%] absolute top-0 left-0 flex flex-col justify-center items-center ">
               <div className="text-2xl text-center font-semibold">
                 Go pro and receive these great benefits:
               </div>
@@ -18,10 +18,11 @@ export default async function Getplus() {
               <div>Unlimited access to dozens of masterclasses</div>
               <div className="pb-6">Unlimited Game Reviews</div>
             </div>
-            <div>
+            <div className="bg-opaque absolute bottom-0 left-0 h-[40%] w-[40%] flex flex-col justify-center items-center space-y-4">
               <div className="font-semibold">For only</div>
               <div className="font-bold">
-                <span className="line-through">$9.99/month</span> FREE
+                <span className="line-through">$9.99/month</span> FREE for ogs
+                dans
               </div>
               <div className="bg-blue-400 px-12 py-4 rounded-md font-semibold cursor-pointer">
                 SUBSCRIBE NOW
