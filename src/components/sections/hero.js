2c1aa691c4950a5c2417327fef97b206b96c0b26
hnakae <nakae27@gmail.com>
2023-06-06 10:53:49 -0700

    signin button

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -34,7 +34,7 @@ const Hero = ({ session }) => {
           className="py-2 px-4 rounded-md placeholder-slate-600 focus:outline-none focus:ring-2 focus:ring-blue-500 w-72"
         /> */}
         <div className=" bg-blue-500 hover:bg-blue-600 cursor-pointer px-12 py-2 rounded-3xl text-white ">
-          {!!session ? <LogoutButton /> : <LoginButton />}
+          {session ? <LogoutButton /> : <LoginButton />}
         </div>
       </div>
     </div>
