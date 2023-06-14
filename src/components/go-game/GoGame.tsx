31688531ca5741bc8fc4e1534621b60f8cb0bca2
hnakae <nakae27@gmail.com>
2023-06-14 09:49:29 -0700

    navigate forward and back

--- src/components/go-game/GoGame.tsx
+++ src/components/go-game/GoGame.tsx
@@ -45,32 +45,26 @@ type BoardArray = Array<Array<JSX.Element | null>>;
 // };
 
 export const GoGame = () => {
-  // const PreloadedImage = ({ src }: any) => {
-  //   return (
-  //     <Image src={src} alt="preloaded image" width={20} height={20} priority />
-  //   );
-  // };
-  // const preloadImages = async () => {
-  //   await Promise.all([
-  //     <PreloadedImage key="image1" src="/black.webp" />,
-  //     <PreloadedImage key="image2" src="/white.webp" />,
-  //   ]);
-  // };
-  // preloadImages();
-
   const boardSize = 19;
   const initialBoard = Array.from({ length: boardSize }, () =>
     Array.from({ length: boardSize }, () => null)
   );
   const [board, setBoard] = useState<BoardArray>(initialBoard);
   const [player, setPlayer] = useState<string>("/black.webp");
+  const [moveHistory, setMoveHistory] = useState<Array<[number, number]>>([]);
+  const [redoMoveHistory, setRedoMoveHistory] = useState<
+    Array<[number, number]>
+  >([]);
 
   const handleOnClick = (row: number, col: number) => {
     // if (board[row][col] || winner) {
     if (board[row][col]) {
       return;
     }
-
+    // Update move history
+    setMoveHistory([...moveHistory, [row, col]]);
+    // Clear redo move history when a new move is made
+    setRedoMoveHistory([]);
     const updatedPlayerBoard = board.map((newRow, rowIndex) =>
       newRow.map((cell, cellIndex) =>
         rowIndex === row && cellIndex === col ? (
@@ -96,6 +90,71 @@ export const GoGame = () => {
     }
   };
 
+  const undoMove = () => {
+    // Check if there are any moves in the move history
+    if (moveHistory.length === 0) {
+      return;
+    }
+
+    // Get the coordinates of the last move
+    const lastMove = moveHistory[moveHistory.length - 1];
+    const [lastMoveRow, lastMoveCol] = lastMove;
+
+    // Clear the last move from the board
+    const updatedBoard = board.map((newRow, rowIndex) =>
+      newRow.map((cell, colIndex) =>
+        rowIndex === lastMoveRow && colIndex === lastMoveCol ? null : cell
+      )
+    );
+    setBoard(updatedBoard);
+
+    // Remove the last move from the move history
+    setMoveHistory(moveHistory.slice(0, -1));
+
+    // Add the last move to the redo move history
+    setRedoMoveHistory([...redoMoveHistory, lastMove]);
+  };
+
+  const redoMove = () => {
+    // Check if there are any moves in the redo move history
+    if (redoMoveHistory.length === 0) {
+      return;
+    }
+
+    // Get the coordinates of the next move to redo
+    const nextMove = redoMoveHistory[redoMoveHistory.length - 1];
+    const [nextMoveRow, nextMoveCol] = nextMove;
+
+    // Toggle the player's color
+    const nextPlayer = player === "/black.webp" ? "/white.webp" : "/black.webp";
+
+    // Set the next move on the board
+    const updatedBoard = board.map((newRow, rowIndex) =>
+      newRow.map((cell, colIndex) =>
+        rowIndex === nextMoveRow && colIndex === nextMoveCol ? (
+          <div
+            key={colIndex}
+            className="shadow-sm shadow-dark rounded-full w-[20px] h-[20px]"
+          >
+            <Image src={nextPlayer} width={20} height={20} alt="img" priority />
+          </div>
+        ) : (
+          cell
+        )
+      )
+    );
+    setBoard(updatedBoard);
+
+    // Add the next move to the move history
+    setMoveHistory([...moveHistory, nextMove]);
+
+    // Remove the next move from the redo move history
+    setRedoMoveHistory(redoMoveHistory.slice(0, -1));
+
+    // Update the player's color
+    setPlayer(nextPlayer);
+  };
+
   const restartGame = () => {
     setBoard(initialBoard);
     setPlayer("/black.webp");
@@ -103,7 +162,7 @@ export const GoGame = () => {
 
   return (
     <div className="game">
-      <h1> Go Game</h1>
+      <h1 className="text-center mb-6"> Go Game</h1>
 
       {/* preload */}
       <Image
@@ -135,13 +194,27 @@ export const GoGame = () => {
           <Board board={board} handleClick={handleOnClick} />
         </div>
       </div>
-      <button
-        className="reset outline"
-        type="button"
-        onClick={() => restartGame()}
-      >
-        Clear Board
-      </button>
+      <div className="flex justify-around items-center outline mt-4  p-4">
+        <button
+          className="hover:underline"
+          type="button"
+          onClick={() => restartGame()}
+        >
+          Clear Board
+        </button>
+        <button className="undo" type="button" onClick={undoMove}>
+          <Image
+            src="/right-arrow.png"
+            alt="img"
+            width={20}
+            height={20}
+            className="transform rotate-180"
+          />
+        </button>
+        <button className="redo" type="button" onClick={redoMove}>
+          <Image src="/right-arrow.png" alt="img" width={20} height={20} />
+        </button>
+      </div>
     </div>
   );
 };
