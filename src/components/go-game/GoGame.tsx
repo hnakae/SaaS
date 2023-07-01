7c1aefe8abdb27ea6cf31b3d1ae1481ed2310936
hnakae <nakae27@gmail.com>
2023-07-01 11:43:52 -0700

    stone group

--- src/components/go-game/GoGame.tsx
+++ src/components/go-game/GoGame.tsx
@@ -1,5 +1,6 @@
-"use client";
-import React, { useState, useEffect } from "react";
+"use client"; // Use client
+
+import React, { useState } from "react";
 import { Board } from "./board";
 import { SvgBoard } from "../../../public/svg/SvgBoard";
 import "./GoGame.sass";
@@ -15,23 +16,23 @@ type Color = "black" | "white";
 type Stone = {
   coordinate: Coordinate;
   color: Color;
+  group: StoneGroup | null;
 };
 
-type Group = {
+type StoneGroup = {
   stones: Stone[];
+  libertyCount: number;
 };
 
-// type BoardArray = Array<Array<JSX.Element | null>>;
 type BoardArray = Array<Array<Stone | null>>;
 
 export const GoGame = () => {
   const boardSize = 19;
-  const initialBoard = Array.from(
-    { length: boardSize },
-    () => Array.from({ length: boardSize }, () => null) //initialize with nulls
+  const initialBoard = Array.from({ length: boardSize }, () =>
+    Array.from({ length: boardSize }, () => null)
   );
   const [board, setBoard] = useState<BoardArray>(initialBoard);
-  const [groups, setGroups] = useState<Group[]>([]);
+  const [groups, setGroups] = useState<StoneGroup[]>([]);
   const [player, setPlayer] = useState<string>("/black.webp");
 
   const placeStone = (row: number, col: number) => {
@@ -41,35 +42,19 @@ export const GoGame = () => {
     const color: Color = player === "/black.webp" ? "black" : "white";
 
     const stone: Stone = {
-      coordinate: { x: col+1, y: row+1 },
+      coordinate: { x: col + 1, y: row + 1 },
       color,
+      group: null,
     };
 
-    const adjacentGroups = findAdjacentGroups(stone);
+    const newGroup: StoneGroup = {
+      stones: [stone],
+      libertyCount: 4,
+    };
+    stone.group = newGroup;
+    setGroups([...groups, newGroup]);
 
-    if (adjacentGroups.length > 0) {
-      // Add stone to the first adjacent group
-      const groupIndex = groups.indexOf(adjacentGroups[0]);
-      const updatedGroups = [...groups];
-      updatedGroups[groupIndex].stones.push(stone);
-      setGroups(updatedGroups);
-
-      console.log("Added stone to existing group:", stone);
-      console.log("Updated groups:", updatedGroups);
-    } else {
-      // Create a new group
-      const newGroup: Group = {
-        stones: [stone],
-      };
-      setGroups([...groups, newGroup]);
-      console.log("Created new group with stone:", stone);
-      console.log("Updated groups:", [...groups, newGroup]);
-    }
-
-    console.log("Placed stone at coordinates:", col + 1, row + 1);
-
-    // render stone
-    const updatedPlayerBoard = board.map((newRow, rowIndex) =>
+    const updatedPlayerBoard: BoardArray = board.map((newRow, rowIndex) =>
       newRow.map((cell, cellIndex) => {
         if (rowIndex === row && cellIndex === col) {
           return stone;
@@ -81,47 +66,22 @@ export const GoGame = () => {
     const nextPlayer = player === "/black.webp" ? "/white.webp" : "/black.webp";
     setPlayer(nextPlayer);
     setBoard(updatedPlayerBoard);
-  };
 
-  const findAdjacentGroups = (stone: Stone): Group[] => {
-    const { x, y } = stone.coordinate;
-    const adjacentGroups: Group[] = [];
-
-    const adjacentCoordinates: Coordinate[] = [
-      { x: x - 1, y },
-      { x: x + 1, y },
-      { x, y: y - 1 },
-      { x, y: y + 1 },
-    ];
-
-    for (const group of groups) {
-      for (const adjacentCoord of adjacentCoordinates) {
-        const { x: adjX, y: adjY } = adjacentCoord;
-
-        const adjacentStone = board[adjX]?.[adjY];
-        console.log("adjacent stone: ", adjacentStone);
-        if (
-          adjacentStone &&
-          adjacentStone.color === stone.color &&
-          group.stones.includes(adjacentStone)
-        ) {
-          adjacentGroups.push(group);
-          break;
-        }
-      }
-    }
-
-    return adjacentGroups;
+    console.log("Stone placed:", stone);
+    console.log("New group created:", newGroup);
+    console.log("Updated board:", updatedPlayerBoard);
+    console.log("Next player:", nextPlayer);
   };
 
   const restartGame = () => {
     setBoard(initialBoard);
     setPlayer("/black.webp");
+
+    console.log("Game restarted");
   };
 
   return (
     <div className="game">
-      {/* preload images lol*/}
       <Image
         src="/black.webp"
         width={20}
@@ -139,22 +99,17 @@ export const GoGame = () => {
         className="hidden"
       />
 
-      {/* container */}
       <div className="flex">
         <div>
-          {/* board */}
           <div className="flex justify-center items-center relative w-[380px] h-[380px] ">
-            {/* Board */}
             <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 z-0 ">
               <SvgBoard />
             </div>
 
-            {/* Stones */}
             <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 ">
               <Board board={board} handleClick={placeStone} />
             </div>
           </div>
-          {/* buttons */}
           <div className="flex justify-around items-center outline mt-4  p-4">
             <button
               className="hover:underline"
--- src/components/go-game/board.tsx
+++ src/components/go-game/board.tsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// // import "./tic-tac-toe.sass";
-
-// type BoardProps = {
-//   board: Array<Array<JSX.Element | null>>;
-//   handleClick: (row: number, col: number) => void;
-// };
-
-// export const Board = ({ board, handleClick }: BoardProps) => {
-//   return (
-//     <div className="board ">
-//       {board.map((row, rowIndex) => (
-//         <div key={rowIndex} className="board_row">
-//           {row.map((cell, cellIndex) => (
-//             <button
-//               key={cellIndex}
-//               // className={`cell ${cell ? `cell_${cell.toLowerCase()}` : ""}`}
-//               className="cell"
-//               onClick={() => handleClick(rowIndex, cellIndex)}
-//             >
-//               {cell}
-//             </button>
-//           ))}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 import Image from "next/image";
 import React from "react";
 
@@ -36,13 +8,19 @@ type Coordinate = {
 
 type Color = "black" | "white";
 
+type StoneGroup = {
+  stones: Stone[];
+  libertyCount: number;
+};
+
 type Stone = {
   coordinate: Coordinate;
   color: Color;
+  group: StoneGroup | null;
 };
 
 type BoardProps = {
-  board: Array<Array<Stone | null>>;
+  board: (Stone | null)[][];
   handleClick: (row: number, col: number) => void;
 };
 
@@ -51,25 +29,28 @@ export const Board = ({ board, handleClick }: BoardProps) => {
     <div className="board">
       {board.map((row, rowIndex) => (
         <div key={rowIndex} className="board_row">
-          {row.map((cell, cellIndex) => (
+          {row.map((stone, colIndex) => (
             <button
-              key={cellIndex}
+              key={`${rowIndex}-${colIndex}`}
               className="cell"
-              onClick={() => handleClick(rowIndex, cellIndex)}
+              onClick={() => handleClick(rowIndex, colIndex)}
             >
-              {cell ? (
+              {stone ? (
                 <div
                   className={`shadow-sm shadow-dark rounded-full w-[20px] h-[20px]`}
                 >
                   <div className="stone-content relative ">
                     <Image
                       src={
-                        cell.color === "black" ? "/black.webp" : "/white.webp"
+                        stone.color === "black" ? "/black.webp" : "/white.webp"
                       }
                       width={20}
                       height={20}
                       alt="img"
                     />
+                    <div className="adjacent-sides absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-blue-600">
+                      {stone.group?.libertyCount}
+                    </div>
                   </div>
                 </div>
               ) : null}
