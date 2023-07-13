f4871e5738fc4adbcfdbafd32fe8e0dd71e59a23
hnakae <nakae27@gmail.com>
2023-07-13 13:30:55 -0700

    remove motion from logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 // const MotionLink = motion(Link);
 
